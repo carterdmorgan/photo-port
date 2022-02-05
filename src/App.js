680eb514ee33d4bc2685388c362162627d44366b
Carter Morgan <carterdmorgan@gmail.com>
2022-02-05 18:18:05 -0500

    handling submit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import About from './components/About';
 import Gallery from './components/Gallery';
 import Nav from './components/Nav';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -24,14 +25,13 @@ function App() {
         currentCategory={currentCategory}>
       </Nav>
       <main>
-        <div>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        <ContactForm></ContactForm>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
-  
+
 }
 
 export default App;
